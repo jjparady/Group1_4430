508d6dd7a85f90ebb6f7fff36255a935c87ff9ee
keithds13 <100040294+keithds13@users.noreply.github.com>
2022-04-29 20:04:28 -0600

    Update app.js

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ const authenticated_menu=[
     //This menu item allows the user to logout
     {label:"Logout",function:"logout()", home:"Logout"},
     //This menu item builds a sub menu that provides the user with the functionality to request time off and see their requests
-    {label:"Time Off",id:"menu1",menu:[
-        {label:"Request Time Off",function:"navigate({fn:'request_time_off'})"},
-        {label:"My Requests",function:"navigate({fn:'show_time_off'})"},
-    ]},
+    //{label:"Time Off",id:"menu1",menu:[
+        //{label:"Request Time Off",function:"navigate({fn:'request_time_off'})"},
+        //{label:"My Requests",function:"navigate({fn:'show_time_off'})"},
+    //]},
     //This menu item allows the user to add additional users. Note the "roles" property of the object. Only users with the role of "manager", "owner", or "administrator" will see this menu item. User roles are not heirachical. All user types you wish to see a menu item must be listed in the elements of the array.
     {label:"Create Account",function:"navigate({fn:'create_account'})", roles:["manager","owner","administrator"]},
     //This menu item adds the menu item for updating an inventory count. Notice how a parameter is passed to the "ice_cream_inventory" function
@@ -50,8 +50,8 @@ const authenticated_menu=[
     //{label:"Ice Cream Inventory Summary",home:"Inventory",function:"navigate({fn:'ice_cream_inventory',params:{style:'summary'}})", roles:["owner","administrator"]},
     {label:"Student List",function:"navigate({fn:'employee_list'})"},
     {label:"Admin Tools",id:"menu2", roles:["manager","owner","administrator"], menu:[
-        {label:"Update Student",function:"update_user()",panel:"update_user"},
-        {label:"Archive Inventory",function:"navigate({fn:'archive_inventory'})"},
+        {label:"Update User",function:"update_user()",panel:"update_user"},
+        //{label:"Archive Inventory",function:"navigate({fn:'archive_inventory'})"},
     ]},
     {label:"Behavioral Tasks",function:"navigate({fn:'show_task_list_behavioral'})"},
     {label:"Family Medicine 1 Tasks",function:"navigate({fn:'show_task_list_family'})"},
@@ -691,7 +691,6 @@ async function employee_list(){
     <tr>
     <th>First Name</th>
     `]
-    header.push("</tr>")
     header.push(`<th>Last Name</th>`)
     header.push(`<th>Email</th>`)
     header.push(`<th>Phone</th>`)
